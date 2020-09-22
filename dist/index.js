334c3f624b5a9841d2926dff1c3e48d8efd9f15a
Zachary Eisinger <zeisinger@github.com>
2020-09-22 08:49:47 -0700

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -16971,7 +16971,6 @@ class DotnetCoreInstaller {
             const response = yield httpClient.getJson(DotNetCoreIndexUrl);
             const result = response.result || {};
             let releasesInfo = result['releases-index'];
-            console.log(versionParts);
             releasesInfo = releasesInfo.filter((info) => {
                 // channel-version is the first 2 elements of the version (e.g. 2.1), filter out versions that don't match 2.1.x.
                 const sdkParts = info['channel-version'].split('.');
