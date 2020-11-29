72ed77e49ec464b2a75a69e7cde33dd8c93c0996
SeppePeeters <38523632+SeppePeeters@users.noreply.github.com>
2020-11-29 20:06:06 +0100

    test

---
+++ WindowsServer/WindowsServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WindowsServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey");
+            Console.WriteLine("*************************");
+            Console.WriteLine();
+            Console.WriteLine("Mijn naam is Seppe Peeters en ik volg de richting AI.");
+            Console.ReadLine();
+        }
+    }
+}
