7669e56997538516b130e5236ae34381e3e8a578
Vladimir Safonkin <vsafonkin@github.com>
2021-02-03 15:31:11 +0300

    Debug

--- __tests__/clear-toolcache.ps1
+++ __tests__/clear-toolcache.ps1
@@ -2,9 +2,11 @@ Write-Host $args[0]
 
 $os = $args[0]
 
-$linuxDotnetPaths = @("/usr/share/dotnet")
-$macOSDotnetPaths = @("/Users/runner/.dotnet")
-$windowsDotnetPaths = @("$env:LocalAppData\Microsoft\dotnet/*", "$env:ProgramFiles\dotnet/*")
+$linuxDotnetPaths = @("/usr/share/dotnet", "$env:HOME/.dotnet")
+$macOSDotnetPaths = @("$env:HOME/.dotnet")
+$windowsDotnetPaths = @("$env:LocalAppData\Microsoft\dotnet/*",
+                        "$env:ProgramFiles\dotnet/*",
+                        "$env:HOME\.dotnet")
 
 $pathsToClear = @()
 
@@ -13,6 +15,7 @@ if ($os -eq "Linux") {
 } elseif ($os -eq "macOS") {
     $pathsToClear = $macOSDotnetPaths
 } elseif ($os -eq "Windows") {
+    Write-Host $env:LocalAppData
     $pathsToClear = $windowsDotnetPaths
 }
 
