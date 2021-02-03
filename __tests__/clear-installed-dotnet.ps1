45c0959fcd4e047c42b807e38dd2d3d0bc3cccf2
Vladimir Safonkin <vsafonkin@github.com>
2021-02-03 16:24:26 +0300

    Debug

--- __tests__/clear-installed-dotnet.ps1
+++ __tests__/clear-installed-dotnet.ps1
@@ -20,7 +20,7 @@ if ($os -eq "Linux") {
 foreach ($path in $pathsToClear) {
     if (Test-Path $path) {
         Write-Host "Clear $path path"
-        Remove-Item $path -Recurse -Force
+        # Remove-Item $path -Recurse -Force
     }
 }
 
