a3d47e556cc06347e059f4db4672c7a8dce29e8b
Vladimir Safonkin <vsafonkin@github.com>
2021-02-03 15:13:32 +0300

    Debug

--- __tests__/clear-toolcache.ps1
+++ __tests__/clear-toolcache.ps1
@@ -1,4 +1,26 @@
 Write-Host $args[0]
 
-Write-Host $env:PATH
+$os = $args[0]
+
+$linuxDotnetPaths = @("/usr/share/dotnet")
+$macOSDotnetPaths = @("/Users/runner/.dotnet")
+$windowsDotnetPaths = @("$env:LocalAppData\Microsoft\dotnet/*", "$env:ProgramFiles\dotnet/*")
+
+$pathsToClear = @()
+
+if ($os == "linux") {
+    $pathsToClear = $linuxDotnetPaths
+} elseif ($os == "macOS") {
+    $pathsToClear = $macOSDotnetPaths
+} elseif ($os == "windows") {
+    $pathsToClear = $windowsDotnetPaths
+}
+
+foreach ($path in $pathsToClear) {
+    if (Test-Path $path) {
+        Write-Host "Clear $path"
+        Remove-Item $path -Recurse -Force
+    }
+}
+
 dotnet --info
