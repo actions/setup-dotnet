059f1aa99bb356fcf96be16c36994328d6294967
Vladimir Safonkin <vsafonkin@github.com>
2021-02-04 09:57:09 +0300

    Debug

--- __tests__/verify-dotnet.ps1
+++ __tests__/verify-dotnet.ps1
@@ -3,11 +3,6 @@ if (!$args[0])
   throw "Must supply dotnet version argument"
 }
 
-if (-Not (Test-Path "../nuget.config"))
-{
-  throw "nuget file not generated correctly"
-}
-
 $dotnet = Get-Command dotnet | Select-Object -First 1 | ForEach-Object { $_.Path }
 Write-Host "Found '$dotnet'"
 
