fabe7d52fed84a7ca9f0af153967e72999011ffe
Vladimir Safonkin <vsafonkin@github.com>
2021-02-04 11:06:13 +0300

    Debug

--- __tests__/verify-dotnet.ps1
+++ __tests__/verify-dotnet.ps1
@@ -8,7 +8,7 @@ Write-Host "Found '$dotnet'"
 
 $version = & $dotnet --version | Out-String | ForEach-Object { $_.Trim() }
 Write-Host "Version $version"
-if ($version -contains $args[0])
+if ($version -notmatch $args[0])
 {
   Write-Host "PATH='$env:path'"
   throw "Unexpected version"
@@ -19,7 +19,7 @@ if ($args[1])
   # SDKs are listed on multiple lines with the path afterwards in square brackets
   $version = & $dotnet --list-sdks | ForEach-Object { $_.SubString(0, $_.IndexOf('[')).Trim() }
   Write-Host "Version $version"
-  if (-not ($version -contains $args[1]))
+  if ($version -notmatch $args[1])
   {
     Write-Host "PATH='$env:path'"
     throw "Unexpected version"
