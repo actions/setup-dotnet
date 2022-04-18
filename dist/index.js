f078482971ee996a7da79a0b59e838f5049eb263
Owen Smith <owen@omsmith.ca>
2022-04-18 08:25:56 -0400

    add global-json-file input (#276)
    * support specifying global.json location with global-json-file input
    
    * add test workflow jobs for global.json usage
    
    * fix typo in global-json-file description
    
    Co-authored-by: Brian Cristante <33549821+brcrista@users.noreply.github.com>
    
    Co-authored-by: Brian Cristante <33549821+brcrista@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -9115,11 +9115,21 @@ function run() {
             //
             // dotnet-version is optional, but needs to be provided for most use cases.
             // If supplied, install / use from the tool cache.
+            // global-version-file may be specified to point to a specific global.json
+            // and will be used to install an additional version.
             // If not supplied, look for version in ./global.json.
             // If a valid version still can't be identified, nothing will be installed.
             // Proxy, auth, (etc) are still set up, even if no version is identified
             //
             let versions = core.getMultilineInput('dotnet-version');
+            const globalJsonFileInput = core.getInput('global-json-file');
+            if (globalJsonFileInput) {
+                const globalJsonPath = path.join(process.cwd(), globalJsonFileInput);
+                if (!fs.existsSync(globalJsonPath)) {
+                    throw new Error(`The specified global.json file '${globalJsonFileInput}' does not exist`);
+                }
+                versions.push(getVersionFromGlobalJson(globalJsonPath));
+            }
             if (!versions.length) {
                 // Try to fall back to global.json
                 core.debug('No version found, trying to find version from global.json');
