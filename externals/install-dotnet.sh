5d7bc0454bd50f1c67f0517bac6bc1fd8944b8e3
Evgenii Korolevskii <e-korolevskii@github.com>
2022-08-31 16:01:51 +0200

    update installers

--- externals/install-dotnet.sh
+++ externals/install-dotnet.sh
@@ -306,6 +306,10 @@ get_machine_architecture() {
             echo "arm64"
             return 0
             ;;
+        s390x)
+            echo "s390x"
+            return 0
+            ;;
         esac
     fi
 
@@ -337,6 +341,10 @@ get_normalized_architecture_from_architecture() {
             ;;
         arm64)
             echo "arm64"
+            return 0
+            ;;
+        s390x)
+            echo "s390x"
             return 0
             ;;
     esac
@@ -1587,7 +1595,7 @@ do
             echo "      -InstallDir"
             echo "  --architecture <ARCHITECTURE>      Architecture of dotnet binaries to be installed, Defaults to \`$architecture\`."
             echo "      --arch,-Architecture,-Arch"
-            echo "          Possible values: x64, arm, and arm64"
+            echo "          Possible values: x64, arm, arm64 and s390x"
             echo "  --os <system>                    Specifies operating system to be used when selecting the installer."
             echo "          Overrides the OS determination approach used by the script. Supported values: osx, linux, linux-musl, freebsd, rhel.6."
             echo "          In case any other value is provided, the platform will be determined by the script based on machine configuration."
