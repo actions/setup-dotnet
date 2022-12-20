11aaa65761eeacbe28d60e8c910ebadcd22028c8
IvanZosimov <ivanzosimov@github.com>
2022-12-20 16:24:40 +0100

    Fix review points

--- __tests__/verify-no-unstaged-changes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$(git diff --ignore-space-at-eol dist/ | wc -l)" -gt "0" ]; then
-    echo "Detected uncommitted changes after build.  See status below:"
-    git diff
-    exit 1
-fi
