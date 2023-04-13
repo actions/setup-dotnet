b72f430d3615f3631ecde8d268101c3804566f79
IvanZosimov <ivanzosimov@github.com>
2023-04-13 17:28:59 +0200

    Update e2e tests

---
+++ __tests__/e2e-test-csproj/Test.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_csproj
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.AreEqual((1 + 1), 2);
+        }
+    }
+}
--- __tests__/verify-dotnet.ps1
+++ __tests__/verify-dotnet.ps1
@@ -1,73 +1,96 @@
-if (!$args[0])
-{
-  throw "Must supply dotnet version argument"
-}
+param(
+  [ValidateNotNullOrEmpty()]
+  [string[]]$Patterns,
+  [ValidateNotNullOrEmpty()]
+  [string]$ContainedPattern,
+  [switch]$CheckNugetConfig
+)
 
-$dotnet = Get-Command dotnet | Select-Object -First 1 | ForEach-Object { $_.Path }
-Write-Host "Found '$dotnet'"
-
-if($args.count -eq 1)
-{
-  $version = & $dotnet --version | Out-String | ForEach-Object { $_.Trim() }
-  Write-Host "Version $version"
-  if (-not ($version.StartsWith($args[0].ToString())))
+if ($CheckNugetConfig.IsPresent) {
+  if (!(Test-Path "../nuget.config"))
   {
-    Write-Host "PATH='$env:PATH'"
-    throw "Unexpected version"
+    throw "The nuget.config file is not generated correctly."
   }
 }
 
-if ($args[1])
+if (!$Patterns.Count)
 {
-  # SDKs are listed on multiple lines with the path afterwards in square brackets
-  $versions = & $dotnet --list-sdks | ForEach-Object { $_.SubString(0, $_.IndexOf('[')).Trim() }
-  Write-Host "Installed versions: $versions"
-  $InstalledVersionCount = 0
-  foreach($arg in $args){
-    foreach ($version in $versions)
+  throw "At least 1 dotnet-version pattern should be supplied to script."
+}
+
+Write-Host "Those patterns were supplied to the script: $($Patterns -join ', ')."
+$dotnet = Get-Command dotnet | Select-Object -First 1 | ForEach-Object { $_.Path }
+Write-Host "Found: '$dotnet'"
+
+# SDKs are listed on multiple lines with the path afterwards in square brackets
+$versions = & $dotnet --list-sdks | ForEach-Object { $_.SubString(0, $_.IndexOf('[')).Trim() }
+Write-Host "Installed versions: $($versions -join ', ')."
+$InstalledVersionCount = 0
+foreach($pattern in $Patterns)
+{
+  foreach ($version in $versions)
+  {
+    if ($ContainedPattern) 
     {
-      if ($version.StartsWith($arg.ToString())) 
+      if ($version.StartsWith($pattern.ToString()) -and $version.Contains($ContainedPattern))
       {
         $InstalledVersionCount++
+      } 
+    } 
+      elseif ($version.StartsWith($pattern.ToString()))
+      {
+          $InstalledVersionCount++
       }
-    }
-   }
-  if ( $InstalledVersionCount -ne $args.Count)
-  {
-    Write-Host "PATH='$env:PATH'"
-    throw "Unexpected version"
   }
+}
+if ( $InstalledVersionCount -ne $Patterns.Count)
+{
+  throw "An unexpected version of Dotnet is found on the machine, please check the script's dotnet-version patterns."
 }
 
-Write-Host "Building sample csproj"
-& $dotnet build __tests__/sample-csproj/ --no-cache
-if ($LASTEXITCODE -ne 0)
+Write-Host "Changing directory to the ./__tests__/e2e-test-csproj"
+Set-Location ./__tests__/e2e-test-csproj
+
+$targetFrameworkVersionMapping = @{
+  "1.0" = "netcoreapp1.0";
+  "1.1" = "netcoreapp1.1";
+  "2.0" = "netcoreapp2.0";
+  "2.1" = "netcoreapp2.1";
+  "2.2" = "netcoreapp2.2";
+  "3.0" = "netcoreapp3.0";
+  "3.1" = "netcoreapp3.1";
+  "5.0" = "net5.0";
+  "6.0" = "net6.0";
+  "7.0" = "net7.0";
+ }
+
+foreach ($version in $versions)
 {
-  throw "Unexpected exit code $LASTEXITCODE"
+  Write-Host "Creating temporary global.json file for $version .NET version."
+  & $dotnet new globaljson --sdk-version $version --force
+  Write-Host "The global.json file for the version $version is created. Currently used .NET version is: $(& $dotnet --version)"
+  $version -match "^(?<key>\d+\.\d+)"
+  Write-Host "Setting the TEST_TARGET_FRAMEWORK environment variable to $($targetFrameworkVersionMapping[$Matches.key])"
+  [Environment]::SetEnvironmentVariable('TEST_TARGET_FRAMEWORK', $($targetFrameworkVersionMapping[$Matches.key]))
+
+  Write-Host "Building test C# project with $version .NET version."
+  & $dotnet build --no-cache
+  if ($LASTEXITCODE -ne 0)
+  {
+    throw "Building process is not successful, exit code: $LASTEXITCODE"
+  }
+
+  Write-Host "Testing compiled C# project with $version .NET version."
+  & $dotnet test --no-build
+  if ($LASTEXITCODE -ne 0)
+  {
+    throw "Testing process is not successful, exit code: $LASTEXITCODE"
+  }
+
+  Write-Host "Tests are completed successfully!"
+
+  Write-Host "Removing temprary global.json file."
+  Remove-Item ./global.json
 }
 
-Write-Host "Testing compiled app"
-$sample_output = "$(dotnet test __tests__/sample-csproj/ --no-build)"
-Write-Host "Sample output: $sample_output"
-# For Side-by-Side installs we want to run the tests twice, for a single install the tests will run once
-if ($args[1])
-{
-  if ($sample_output -notlike "*Test Run Successful.*Test Run Successful.*")
-  {
-    throw "Unexpected output"
-  }
-}
-if ($args[2])
-{
-  if ($sample_output -notlike "*Test Run Successful.*Test Run Successful.*Test Run Successful.*")
-  {
-    throw "Unexpected output"
-  }
-}
-else
-{
-  if ($sample_output -notlike "*Test Run Successful.*")
-  {
-    throw "Unexpected output"
-  }
-}
+Set-Location ../..
